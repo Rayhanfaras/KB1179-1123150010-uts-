f1b186b761bee360d34605064f2f755c7ce7edf5
Rayhan Faras <ryhanfrasrmdhani@gmail.com>
2025-11-09 00:50:20 +0700

    Menambahkan 3 bullet

--- lib/screen/splash_screen_1.dart
+++ lib/screen/splash_screen_1.dart
@@ -36,7 +36,39 @@ class Screen1 extends StatelessWidget {
             color: Colors.black
           )),
 
+SizedBox(height: 5),
 
+          Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: 10,
+                            width: 10,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.green,
+                            ),
+                          ),
+                          SizedBox(width: 5),
+                          Container(
+                            height: 10,
+                            width: 10,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Color(0xFFC8E6C9),
+                            ),
+                          ),
+                          SizedBox(width: 5),
+                          Container(
+                            height: 10,
+                            width: 10,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Color(0xFFC8E6C9),
+                            ),
+                          ),
+                        ],
+                      ),
 
 
          ], 
---
+++ lib/screen/splash_screen_2.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+//import 'package:splash_screen/screen/splash_screen2.dart';
+
+class Screen1 extends StatelessWidget {
+  const Screen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+         mainAxisAlignment: MainAxisAlignment.center,
+         children: [
+          // Spacer(),
+          const SizedBox(height: 25),
+          Container(
+            height: 200,
+            width: 200,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: const Color.fromARGB(255, 104, 116, 104),
+              image: DecorationImage(image: AssetImage("assets/images/image.png"),
+              fit: BoxFit.cover,
+              )
+            ),
+          ),
+          SizedBox(height: 20),
+          Text("Welcome",style: TextStyle(
+            fontSize: 30,
+            color: Colors.black
+          )),
+          Text("Forgot to bring your wallet \n when you are shopping?",
+          textAlign: TextAlign.center,
+          style: TextStyle(  
+            fontSize: 12,
+            color: Colors.black
+          )),
+
+
+
+
+         ], 
+        ),
+      ),
+    );
+  }
+}
